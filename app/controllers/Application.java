dd7b24822edc1c3f24f7b69010011921146c865e
Erwan Loisant <eloisant@gmail.com>
2015-01-20 14:44:38 +0100

    Upgrade kit version, switch from OAuth to previews

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import org.joda.time.LocalDate;
 import play.*;
 import play.mvc.*;
 
@@ -32,37 +33,37 @@ public class Application extends Controller {
 
       // For "Bookmarked" documents that use a special page
       if(isBookmark(api, link, "about")) {
-        return routes.Application.about(ref).absoluteURL(request);
+        return routes.Application.about().absoluteURL(request);
       }
       else if(isBookmark(api, link, "jobs")) {
-        return routes.Application.jobs(ref).absoluteURL(request);
+        return routes.Application.jobs().absoluteURL(request);
       }
       else if(isBookmark(api, link, "stores")) {
-        return routes.Application.stores(ref).absoluteURL(request);
+        return routes.Application.stores().absoluteURL(request);
       }
 
       // Store documents
       else if("store".equals(link.getType()) && !link.isBroken()) {
-        return routes.Application.storeDetail(link.getId(), link.getSlug(), ref).absoluteURL(request);
+        return routes.Application.storeDetail(link.getId(), link.getSlug()).absoluteURL(request);
       }
 
       // Any product
       else if("product".equals(link.getType()) && !link.isBroken()) {
-        return routes.Application.productDetail(link.getId(), link.getSlug(), ref).absoluteURL(request);
+        return routes.Application.productDetail(link.getId(), link.getSlug()).absoluteURL(request);
       } 
 
       // Job offers
       else if("job-offer".equals(link.getType()) && !link.isBroken()) {
-        return routes.Application.jobDetail(link.getId(), link.getSlug(), ref).absoluteURL(request);
+        return routes.Application.jobDetail(link.getId(), link.getSlug()).absoluteURL(request);
       } 
 
       // Blog
       else if("blog-post".equals(link.getType()) && !link.isBroken()) {
-        return routes.Application.blogPost(link.getId(), link.getSlug(), ref).absoluteURL(request);
+        return routes.Application.blogPost(link.getId(), link.getSlug()).absoluteURL(request);
       } 
 
       // Any other link
-      return routes.Application.brokenLink(ref).absoluteURL(request);
+      return routes.Application.brokenLink().absoluteURL(request);
     }
   }
 
@@ -72,23 +73,23 @@ public class Application extends Controller {
   }
 
   @Prismic.Action
-  public static Result brokenLink(String ref) {
+  public static Result brokenLink() {
     return pageNotFound();
   }
 
   // -- Home page
 
   @Prismic.Action
-  public static Result index(String ref) {
-    List<Document> products = prismic().getApi().getForm("products").ref(prismic().getRef()).submit();
-    List<Document> featured = prismic().getApi().getForm("featured").ref(prismic().getRef()).submit();
+  public static Result index() {
+    List<Document> products = prismic().getApi().getForm("products").ref(prismic().getRef()).submit().getResults();
+    List<Document> featured = prismic().getApi().getForm("featured").ref(prismic().getRef()).submit().getResults();
     return ok(views.html.index.render(products, featured));
   }
 
   // -- About us
 
   @Prismic.Action
-  public static Result about(String ref) {
+  public static Result about() {
     Document aboutPage = prismic().getBookmark("about");
     if(aboutPage == null) {
       return pageNotFound();
@@ -100,32 +101,32 @@ public class Application extends Controller {
   // -- Jobs
 
   @Prismic.Action
-  public static Result jobs(String ref) {
+  public static Result jobs() {
     Document jobsPage = prismic().getBookmark("jobs");
     if(jobsPage == null) {
       return pageNotFound();
     } else {
-      List<Document> jobs = prismic().getApi().getForm("jobs").ref(prismic().getRef()).submit();
+      List<Document> jobs = prismic().getApi().getForm("jobs").ref(prismic().getRef()).submit().getResults();
       return ok(views.html.jobs.render(jobsPage, jobs));
     }
   }
 
   @Prismic.Action
-  public static Result jobDetail(String id, String slug, String ref) {
+  public static Result jobDetail(String id, String slug) {
     Document jobsPage = prismic().getBookmark("jobs");
     if(jobsPage == null) {
       return pageNotFound();
     } else {
       Document job = prismic().getDocument(id);
       String checked = prismic().checkSlug(job, slug);
-      if(checked == DOCUMENT_NOT_FOUND) {
+      if (DOCUMENT_NOT_FOUND.equals(checked)) {
         return pageNotFound();
       }
       else if(checked == null) {
         return ok(views.html.jobDetail.render(jobsPage, job));
       }
       else {
-        return redirect(routes.Application.jobDetail(id, checked, ref));
+        return redirect(routes.Application.jobDetail(id, checked));
       }
     }
   }
@@ -133,18 +134,18 @@ public class Application extends Controller {
   // -- Stores
 
   @Prismic.Action
-  public static Result stores(String ref) {
+  public static Result stores() {
     Document storesPage = prismic().getBookmark("stores");
     if(storesPage == null) {
       return pageNotFound();
     } else {
-      List<Document> stores = prismic().getApi().getForm("stores").ref(prismic().getRef()).submit();
+      List<Document> stores = prismic().getApi().getForm("stores").ref(prismic().getRef()).submit().getResults();
       return ok(views.html.stores.render(storesPage, stores));
     }
   }
 
   @Prismic.Action
-  public static Result storeDetail(String id, String slug, String ref) {
+  public static Result storeDetail(String id, String slug) {
     Document store = prismic().getDocument(id);
     String checked = prismic().checkSlug(store, slug);
     if(checked == DOCUMENT_NOT_FOUND) {
@@ -154,14 +155,14 @@ public class Application extends Controller {
       return ok(views.html.storeDetail.render(store));
     }
     else {
-      return redirect(routes.Application.storeDetail(id, checked, ref));
+      return redirect(routes.Application.storeDetail(id, checked));
     }
   }
 
   // -- Products Selections
 
   @Prismic.Action
-  public static Result selectionDetail(String id, String slug, String ref) {
+  public static Result selectionDetail(String id, String slug) {
     Document selection = prismic().getDocument(id);
     String checked = prismic().checkSlug(selection, slug);
     if(checked == DOCUMENT_NOT_FOUND) {
@@ -179,7 +180,7 @@ public class Application extends Controller {
       return ok(views.html.selectionDetail.render(selection, products));
     }
     else {
-      return redirect(routes.Application.selectionDetail(id, checked, ref));
+      return redirect(routes.Application.selectionDetail(id, checked));
     }
   }
 
@@ -192,17 +193,17 @@ public class Application extends Controller {
   };
 
   @Prismic.Action
-  public static Result blog(String category, String ref) {
+  public static Result blog(String category) {
     List<Document> posts;
     if(category == null) {
-      posts = prismic().getApi().getForm("blog").ref(prismic().getRef()).submit();
+      posts = prismic().getApi().getForm("blog").ref(prismic().getRef()).submit().getResults();
     } else {
-      posts = prismic().getApi().getForm("blog").query("[[:d = at(my.blog-post.category, \"" + category + "\")]]").ref(prismic().getRef()).submit();
+      posts = prismic().getApi().getForm("blog").query("[[:d = at(my.blog-post.category, \"" + category + "\")]]").ref(prismic().getRef()).submit().getResults();
     }
     Collections.sort(posts, new Comparator<Document>() {
       public int compare(Document d1, Document d2) {
-        Long t1 = d1.getDate("blog-post.date") == null ? 0 : d1.getDate("blog-post.date").getValue().getMillis();
-        Long t2 = d2.getDate("blog-post.date") == null ? 0 : d2.getDate("blog-post.date").getValue().getMillis();
+        Long t1 = d1.getDate("blog-post.date") == null ? 0 : d1.getDate("blog-post.date").getValue().toDate().getTime();
+        Long t2 = d2.getDate("blog-post.date") == null ? 0 : d2.getDate("blog-post.date").getValue().toDate().getTime();
         return t2.compareTo(t1);
       }
     });
@@ -210,7 +211,7 @@ public class Application extends Controller {
   }
 
   @Prismic.Action
-  public static Result blogPost(String id, String slug, String ref) {
+  public static Result blogPost(String id, String slug) {
     Document post = prismic().getDocument(id);
     String checked = prismic().checkSlug(post, slug);
     if(checked == DOCUMENT_NOT_FOUND) {
@@ -238,7 +239,7 @@ public class Application extends Controller {
       return ok(views.html.postDetail.render(post, relatedProducts, relatedPosts));
     }
     else {
-      return redirect(routes.Application.blogPost(id, checked, ref));
+      return redirect(routes.Application.blogPost(id, checked));
     }
   }
 
@@ -252,13 +253,13 @@ public class Application extends Controller {
   }
 
   @Prismic.Action
-  public static Result products(String ref) {
-    List<Document> products = prismic().getApi().getForm("products").ref(prismic().getRef()).submit();
+  public static Result products() {
+    List<Document> products = prismic().getApi().getForm("products").ref(prismic().getRef()).submit().getResults();
     return ok(views.html.products.render(products, null));
   }
 
   @Prismic.Action
-  public static Result productDetail(String id, String slug, String ref) {
+  public static Result productDetail(String id, String slug) {
     Document product = prismic().getDocument(id);
     String checked = prismic().checkSlug(product, slug);
     if(checked == DOCUMENT_NOT_FOUND) {
@@ -277,13 +278,15 @@ public class Application extends Controller {
       return ok(views.html.productDetail.render(product, relatedProducts));
     }
     else {
-      return redirect(routes.Application.productDetail(id, checked, ref));
+      return redirect(routes.Application.productDetail(id, checked));
     }
   }
 
   @Prismic.Action
-  public static Result productsByFlavour(String flavour, String ref) {
-    List<Document> products = prismic().getApi().getForm("everything").query("[[:d = at(my.product.flavour, \"" + flavour + "\")]]").ref(prismic().getRef()).submit();
+  public static Result productsByFlavour(String flavour) {
+    List<Document> products = prismic().getApi().getForm("everything")
+      .query(Predicates.at("my.product.flavour", flavour))
+      .ref(prismic().getRef()).submit().getResults();
     if(products.isEmpty()) {
       return pageNotFound();
     } else {
@@ -294,12 +297,16 @@ public class Application extends Controller {
   // -- Search
 
   @Prismic.Action
-  public static Result search(String query, String ref) {
+  public static Result search(String query) {
     if(query == null || query.trim().isEmpty()) {
       return ok(views.html.search.render(null, null, null));
     } else {
-      List<Document> products = prismic().getApi().getForm("everything").query("[[:d = any(document.type, [\"product\", \"selection\"])][:d = fulltext(document, \"" + query + "\")]]").ref(prismic().getRef()).submit();
-      List<Document> others = prismic().getApi().getForm("everything").query("[[:d = any(document.type, [\"article\", \"blog-post\", \"job-offer\", \"store\"])][:d = fulltext(document, \"" + query + "\")]]").ref(prismic().getRef()).submit();
+      List<Document> products = prismic().getApi().getForm("everything")
+        .query(Predicates.any("document.type", Arrays.asList("product", "selection")), Predicates.fulltext("document", query))
+        .ref(prismic().getRef()).submit().getResults();
+      List<Document> others = prismic().getApi().getForm("everything")
+        .query(Predicates.any("document.type", Arrays.asList("article", "blog-post", "job-offer", "store")), Predicates.fulltext("document", query))
+        .ref(prismic().getRef()).submit().getResults();
       return ok(views.html.search.render(query, products, others));
     }
   }
--- app/controllers/Prismic.java
+++ app/controllers/Prismic.java
@@ -9,6 +9,7 @@ import java.util.*;
 import java.lang.annotation.*;
 
 import io.prismic.*;
+import scala.NotImplementedError;
 
 public class Prismic extends Controller {
 
@@ -48,15 +49,6 @@ public class Prismic extends Controller {
     return value;
   }
 
-  // Compute the callback URL to use for the OAuth worklow
-  public static String callbackUrl(Http.Request request) {
-    String[] referer = request.headers().get("referer");
-    if(referer == null) {
-      referer = new String[0];
-    }
-    return routes.Prismic.callback(null, referer.length > 0 ? referer[0] : null).absoluteURL(request);
-  }
-
   // -- Fetch the API entry document
   public static Api getApiHome(String accessToken) {
     return Api.get(config("prismic.api"), accessToken, CACHE, LOGGER);
@@ -92,20 +84,16 @@ public class Prismic extends Controller {
       return linkResolver;
     }
 
-    public String maybeRef() {
-      if(ref.equals(api.getMaster().getRef())) {
-        return null;
-      }
-      return ref;
-    }
-
     public boolean hasPrivilegedAccess() {
       return accessToken != null;
     }
 
     // -- Helper: Retrieve a single document by Id
     public Document getDocument(String id) {
-      List<Document> results = this.getApi().getForm("everything").query("[[:d = at(document.id, \"" + id + "\")]]").ref(this.getRef()).submit();
+      List<Document> results = this.getApi().getForm("everything")
+        .query(Predicates.at("document.id", id))
+        .ref(this.getRef())
+        .submit().getResults();
       if(results.size() > 0) {
         return results.get(0);
       }
@@ -125,7 +113,7 @@ public class Prismic extends Controller {
           sep = ",";
         }
         q.append("])]]");
-        return this.getApi().getForm("everything").query(q.toString()).ref(this.getRef()).submit();
+        return this.getApi().getForm("everything").query(q.toString()).ref(this.getRef()).submit().getResults();
       }
     }
 
@@ -174,10 +162,14 @@ public class Prismic extends Controller {
     // Reetrieve the API
     Api api = getApiHome(accessToken);
 
-    // Reuse the ref from the incoming request or use master ref
-    String ref = ctx.request().getQueryString("ref");
-    if(ref == null || ref.trim().isEmpty()) {
-      ref = api.getMaster().getRef();
+    // Use the ref from the preview cookie, experiment cookie or master
+    String ref = api.getMaster().getRef();
+    Http.Cookie previewCookie = ctx.request().cookie(io.prismic.Prismic.PREVIEW_COOKIE);
+    Http.Cookie experimentCookie = ctx.request().cookie(io.prismic.Prismic.EXPERIMENTS_COOKIE);
+    if (previewCookie != null) {
+      ref = previewCookie.value();
+    } else if (experimentCookie != null) {
+      ref = api.getExperiments().refFromCookie(experimentCookie.value());
     }
 
     // Create the Prismic context
@@ -195,7 +187,7 @@ public class Prismic extends Controller {
 
   public static class ActionImpl extends play.mvc.Action<Prismic.Action> {
 
-    public F.Promise<SimpleResult> call(Http.Context ctx) throws Throwable {
+    public F.Promise<Result> call(Http.Context ctx) throws Throwable {
       buildContext(ctx);
 
       // Go!
@@ -204,57 +196,15 @@ public class Prismic extends Controller {
 
   }
 
-
   // --
-  // -- OAuth actions
+  // -- Previews
   // --
-
   @Prismic.Action
-  public static Result signin() throws Exception {
-    StringBuilder url = new StringBuilder();
-    url.append(prismic().getApi().getOAuthInitiateEndpoint());
-    url.append("?");
-    url.append("client_id=");
-    url.append(URLEncoder.encode(config("prismic.clientId"), "utf-8"));
-    url.append("&redirect_uri=");
-    url.append(URLEncoder.encode(callbackUrl(ctx().request()), "utf-8"));
-    url.append("&scope=");
-    url.append(URLEncoder.encode("master+releases", "utf-8"));
-    return redirect(url.toString());
-  }
-
-  public static Result signout() {
-    session().clear();
-    return redirect(routes.Application.index(null));
-  }
-
-  @Prismic.Action
-  public static Result callback(String code, String redirect_uri) throws Exception {
-    StringBuilder body = new StringBuilder();
-    body.append("grant_type=");
-    body.append(URLEncoder.encode("authorization_code", "utf-8"));
-    body.append("&code=");
-    body.append(URLEncoder.encode(code, "utf-8"));
-    body.append("&redirect_uri=");
-    body.append(URLEncoder.encode(callbackUrl(ctx().request()), "utf-8"));
-    body.append("&client_id=");
-    body.append(URLEncoder.encode(config("prismic.clientId"), "utf-8"));
-    body.append("&client_secret=");
-    body.append(URLEncoder.encode(config("prismic.clientSecret"), "utf-8")); 
-    WS.Response response = WS.url(prismic().getApi().getOAuthTokenEndpoint()).setHeader(CONTENT_TYPE, "application/x-www-form-urlencoded").post(body.toString()).get();
-    if(response.getStatus() == 200) {
-      String accessToken = response.asJson().path("access_token").asText();
-      String redirectUrl = redirect_uri;
-      if(redirectUrl == null) {
-        redirectUrl = routes.Application.index(null).url();
-      }
-      session(ACCESS_TOKEN, accessToken);
-      return redirect(redirectUrl);
-    }
-    else {
-      play.Logger.of("prismic").error("Can't retrieve the OAuth token for code " + code + ": " + response.getStatus() + " " + response.getBody());
-      return unauthorized("Can't sign you in");
-    }
+  public static Result preview(String token) {
+    String indexUrl = controllers.routes.Application.index().url();
+    String url = prismic().api.previewSession(token, prismic().getLinkResolver(), indexUrl);
+    response().setCookie(io.prismic.Prismic.PREVIEW_COOKIE, token, 1800);
+    return redirect(url);
   }
 
 }
